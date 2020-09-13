ed9cbdce7717c7cfba1fe0220c0419be6bf9397c
Ashley Chica <ahaven11@gmail.com>
2020-09-13 17:06:06 -0400

    made search

---
+++ client/src/pages/Saved.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+
+function Saved() {
+    return (
+        <div>testing testing</div>
+    )
+
+
+}
+
+export default Saved;
---
+++ client/src/pages/Search.js
@@ -0,0 +1,19 @@
+import React from "react";
+
+function Search() {
+    return (
+        <div className="jumbotron jumbotron-fluid">
+            <div className="container">
+                <form>
+                    <label>Book Search</label>
+                    <input type='text'></input>
+                    <button type='button'>Search</button>
+                </form>
+            </div>
+        </div>
+    )
+
+
+};
+
+export default Search;
